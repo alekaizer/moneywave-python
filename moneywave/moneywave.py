52060b479a59c2af80bee81f344e3228d694f399
Achille AROUKO <achille.arouko@gmail.com>
2017-11-18 05:44:30 +0100

    rename Classes

--- moneywave/moneywave.py
+++ moneywave/moneywave.py
@@ -6,17 +6,17 @@ from moneywave.wallet import Wallet
 
 
 class MoneyWave:
-    def __init__(self, api_key, secret_key):
-        self.settings = Settings(api_key, secret_key)
-        self.util = Util(self.settings)
-        self.accounts = Account(self.util)
-        self.resources = Resource(self.util)
-        self.wallets = Wallet(self.util)
-        self.transactions = Transaction(self.util)
+    def __init__(self, api_key, secret_key, mode="test"):
+        self.settings = Settings(api_key, secret_key, mode)
+        self.__util = Util(self.settings)
+        self.Account = Account(self.__util)
+        self.Resource = Resource(self.__util)
+        self.Wallet = Wallet(self.__util)
+        self.Transaction = Transaction(self.__util)
 
     def __get_auth_key(self):
         pass
 
     @property
     def token(self):
-        return self.util.token
+        return self.__util.token
