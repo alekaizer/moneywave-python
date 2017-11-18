8d0df04536c0f434c209e4f8d28fed5894cca445
Achille AROUKO <achille.arouko@gmail.com>
2017-11-18 05:08:43 +0100

    wallet funding

--- moneywave/wallet/funding/funding.py
+++ moneywave/wallet/funding/funding.py
@@ -43,9 +43,29 @@ class Funding:
                                       data)
 
     def card_to_wallet(self, first_name, last_name, phone_number, email,
-                       recipient,
-                       recipient_id, card_no, expiry_year, expiry_month, cvv,
-                       amount, fee, redirect_url, medium="mobile",
-                       narration=None, is_mastercard=False):
-        charge_with = "card"
-        charge_auth = "PIN" if is_mastercard else None
+                       recipient, recipient_id, card_no, expiry_year,
+                       expiry_month, cvv, amount, fee, redirect_url, medium,
+                       narration=None, pin=None):
+        is_verve, is_mastercard = self.card_details(card_no)
+        if is_verve and not pin:
+            raise Exception('Verve cards require PIN value, please set the '
+                            'argument')
+        data = {"firstname": first_name, "lastname": last_name, "email": email,
+                "phonenumber": phone_number, "recipien": recipient,
+                "recipient_id": recipient_id, "card_no": card_no,
+                "expiry_month": expiry_month, "expiry_year": expiry_year,
+                "cvv": cvv, "apiKey": self.util.settings.api_key,
+                "medium": medium,
+                "fee": fee, "amount": amount, "redirecturl": redirect_url}
+        if narration:
+            data['narration'] = narration
+        if is_verve:
+            data['pin'] = pin
+        if is_mastercard:
+            data['charge_with'] = "card"
+            data['charge_auth'] = 'PIN'
+        return self.util.send_request(self.util.settings.funding_pay, data)
+
+    def card_details(self, card_number):
+        data = {"card_no": card_number}
+        return self.util.send_request(self.util.settings.card_enquiry, data)
